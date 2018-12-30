f8fe5ef5ad9568298decf52dde139926fe6c2e10
BoyWithSilverWings <agney@outlook.in>
2018-12-30 19:26:11 +0530

    add a build script

--- src/commands/build.js
+++ src/commands/build.js
@@ -5,9 +5,37 @@ module.exports = {
   name: 'build',
   run: async toolbox => {
     const {
-      filesystem: { read },
+      system: { spawn }, 
     } = toolbox;
-    const config = read('../webpack.config.js', 'json');
-    console.log(config);
+    
+    const config = require("../webpack.config.js");
+    const compiler = webpack(config);
+
+    compiler.run((err, stats) => {
+      if (err) {
+        console.error(err.stack || err);
+        if (err.details) {
+          console.error(err.details);
+        }
+        return;
+      }
+
+      const info = stats.toJson();
+
+      if (stats.hasErrors()) {
+        console.error(info.errors);
+        return;
+      }
+
+      if (stats.hasWarnings()) {
+        console.warn(info.warnings);
+      }
+
+      spawn(`npm run build`, {
+        shell: true,
+        stdio: 'inherit',
+        stderr: 'inherit'
+      });
+    })
   }
 }
